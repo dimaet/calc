bee73411700bd12842ec7fdeb4ffdad4185d6502
dimaet <dimitry04iv@gmail.com>
2024-11-29 14:28:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.getElementById('calculateBtn').addEventListener('click', function() {
-    const ei = document.getElementById('ei').value;
-    const es = document.getElementById('es').value;
-    const x = document.getElementById('x').value;
-    const sigma = document.getElementById('sigma').value;
+    const ei = parseFloat(document.getElementById('ei').value);
+    const es = parseFloat(document.getElementById('es').value);
+    const x = parseFloat(document.getElementById('x').value);
+    const sigma = parseFloat(document.getElementById('sigma').value);
+
 
     const z1 = (es-x)/sigma;
     const z2 = (ei-x)/sigma;
@@ -26,18 +27,19 @@ document.getElementById('calculateBtn').addEventListener('click', function() {
         // Расчет z-значений
         const z3 = (x - 3 * sigma - x) / sigma; // Неисправимый брак
         const z4 = (x + 3 * sigma - x) / sigma; // Исправимый брак
-        
+   
         // Получение значений и расчет процентов
-        const zr1 = F(z1).toFixed(3); // Годные детали
-        const zr2 = F(z2).toFixed(3); // Бракованные детали
-        const zr3 = F(z3).toFixed(3); // Неисправимый брак
-        const zr4 = ((F(z1) - F(z2)) * 100).toFixed(2); // Процент годных к бракованным
-        const zr5 = ((F(z2) - F(z3)) * 100).toFixed(2); // Процент бракованных к неисправимым
-        const zr6 = ((F(z4) - F(z1)) * 100).toFixed(2); // Процент исправимых к годным
+        const zr1 = (F(z1)*100).toFixed(2); // Годные детали
+        const zr2 = (F(z2)*100).toFixed(2); // Бракованные детали
+        const zr3 = (F(z3)*100).toFixed(2); // Неисправимый брак
+        const zr4 = (F(z4)*100).toFixed(2);
+        const zr5 = ((F(z1) - F(z2)) * 100).toFixed(2); // Процент годных к бракованным
+        const zr6 = ((F(z2) - F(z3)) * 100).toFixed(2); // Процент бракованных к неисправимым
+        const zr7 = ((F(z4) - F(z1)) * 100).toFixed(2); // Процент исправимых к годным
     
-        document.getElementById('result1').innerHTML = `Количество годных деталей: ${zr1} - ${zr2} = <strong>${zr4}</strong> %`;
-        document.getElementById('result2').innerHTML = `Количество неисправимого брака: ${zr2} - ${zr3} = <strong>${zr5}</strong> %`;
-        document.getElementById('result3').innerHTML = `Количество исправимого брака: ${zr6} - ${zr1} = <strong>${zr6}</strong> %`;
+        document.getElementById('result1').innerHTML = `Количество годных деталей: ${zr1}% - ${zr2}% = <strong>${zr5}</strong> %`;
+        document.getElementById('result2').innerHTML = `Количество неисправимого брака: ${zr2}% - ${zr3}% = <strong>${zr6}</strong> %`;
+        document.getElementById('result3').innerHTML = `Количество исправимого брака: ${zr4}% - ${zr1}% = <strong>${zr7}</strong> %`;
     } else {
         alert('Заполните все поля!');
     }
